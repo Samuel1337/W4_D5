57713f366b03c2b5342b8900618894dab6a2b77c
Matia Kim <matia.h.kim@gmail.com>
2022-03-18 16:44:41 -0700

    two_sum

--- two_sum.rb
+++ two_sum.rb
@@ -19,9 +19,21 @@ def okay_two_sum?(arr,target_sum)
 end
 
 def two_sum?(arr,target_sum)
-    
+    hash = Hash.new {|h, k| h[k] = [] }
+    # arr.each {|ele| hash[ele] = target_sum - ele}
+    # var = hash.map {|k,v| [k,v] if v != k }.compact
+    # var.any? {|k| arr.include?(k) }
+    arr.each_with_index do |ele, i|
+        hash[i] = [target_sum - ele, ele]
+    end
+    mapped = hash.map {|k,v| v if v.uniq.length != 1 }.compact 
+    # mapped.any? do |subarr|
+        
+    # end
 end
 
+# k target, [target-ele, ele]
+
 arr = [0, 1, 5, 7]
-p okay_two_sum?(arr, 6) # => should be true 
-p okay_two_sum?(arr, 10) # => should be false
+p two_sum?(arr, 6) # => should be true 
+p two_sum?(arr, 10) # => should be false
